f95ada5e2a910f136a8e514caab10f4a73f651d2
Ogunyomi Toluwanimi <odinprojectdevs@gmail.com>
2022-08-14 20:25:59 +0000

    replaced constructor with classes

--- lib.js
+++ lib.js
@@ -11,11 +11,13 @@ removeForm.addEventListener('click', function(){
 
 let myLibrary = []
 
-function book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read
+class book{
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read
+    }
 }
 
 const bookTitle = document.querySelector('#title')
